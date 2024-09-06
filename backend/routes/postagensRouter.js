bb73efb9320483c1feb66713f4f2d08cc2477aae
noname-clang <139624443+noname-clang@users.noreply.github.com>
2024-09-06 07:27:31 -0300

    Update postagensRouter.js

--- backend/routes/postagensRouter.js
+++ backend/routes/postagensRouter.js
@@ -4,13 +4,9 @@ import * as posts from "../controllers/postagensController.js";
 const router = Router();
 
 router.post("/", posts.create);
-<<<<<<< Updated upstream
-// router.get("/", getAll);
-// router.get("/:id", getTarefa);
-=======
 router.get("/", posts.showall);
 router.get("/:id", posts.getbyid);
->>>>>>> Stashed changes
+
 // router.put("/:id", updateTarefa);
 // router.patch("/:id/status", updateStatusTarefa);
 // router.get("/status/:situacao", buscarTarefaPorSituacao);
